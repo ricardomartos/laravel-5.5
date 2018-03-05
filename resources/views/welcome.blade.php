772ba76a4e93ac49c702dfe2548cd60b43cc47aa
Ricardo Martos <r.martos@exponovias.es>
2018-03-05 17:48:46 +0100

    lenguages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    @lang('messages.title')
                 </div>
 
                 <div class="links">
